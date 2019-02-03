a494d12b781b6fb331d0a14eef8a9daec8da7262
Unknown <jonnyrogersanimation@gmail.com>
2019-02-03 12:55:36 -0700

    Created new Unity project for Slope Heroes
    Scripted player to rotate with left and right arrow keys, also made player jump. Added continuous right movement in the x axis but rotation doesn't work right with movement.

---
+++ DGM2610 - Game Design I/Slope_Heroes/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float moveSpeed;
+	public float rotateSpeed;
+	public float jumpSpeed; 
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//transform.Translate(new Vector3(moveSpeed, 0, 0) * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Rotate(new Vector3(0, 0, -rotateSpeed) * Time.deltaTime);
+		}
+		
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Rotate(new Vector3(0, 0, rotateSpeed) * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.Space))
+		{
+			if (transform.position.y <= 0f)
+			{
+				GetComponent<Rigidbody> ().AddForce(Vector3.up * jumpSpeed);
+			}
+			
+		}
+		
+		
+		
+	}
+}
