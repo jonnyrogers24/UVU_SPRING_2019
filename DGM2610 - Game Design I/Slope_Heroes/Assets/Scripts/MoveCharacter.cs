2d6cb6f04042626d063b08d28688a5267dfec325
Unknown <jonnyrogersanimation@gmail.com>
2019-02-28 14:52:10 -0700

    added character movement to player in Slope_heroes

---
+++ DGM2610 - Game Design I/Slope_Heroes/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+
+	public float moveSpeed = 5;
+	public float Gravity = -9.81f; 
+	
+	private CharacterController controller;
+	private Vector3 position;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		position.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+		position.y = Gravity * Time.deltaTime; 
+		
+		controller.Move(position);
+	}
+}
