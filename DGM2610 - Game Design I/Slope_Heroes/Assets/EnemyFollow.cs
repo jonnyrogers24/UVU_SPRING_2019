ac8b936ab1d032a41498012d737e45b2ca6a2f69
Unknown <jonnyrogersanimation@gmail.com>
2019-03-04 21:53:49 -0700

    added scripts to enemy snowball. added triggers to speed up enemy

---
+++ DGM2610 - Game Design I/Slope_Heroes/Assets/EnemyFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+	public Transform target;
+	
+	public float enemySpeed;
+
+	void Update ()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, target.transform.position, enemySpeed * Time.deltaTime);
+	}
+
+}
---
+++ DGM2610 - Game Design I/Slope_Heroes/Assets/SnowballAddSpeed.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballAddSpeed : MonoBehaviour
+{
+    public GameObject Snowball; 
+    private EnemyFollow EnemyFollowScript;
+    public float enemyBoost = 2;  
+    
+    private void OnTriggerEnter(Collider other)
+    {
+                
+      EnemyFollowScript = Snowball.GetComponent<EnemyFollow>();
+      EnemyFollowScript.enemySpeed = EnemyFollowScript.enemySpeed + enemyBoost; 
+              
+    }
+}
