7460c79400ac80d76ea3e209a5162daf4d8e721f
Unknown <jonnyrogersanimation@gmail.com>
2019-03-06 16:06:29 -0700

    added icicles to slow down snowball

---
+++ DGM2610 - Game Design I/Slope_Heroes/Assets/Scripts/IcicleSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEditor.Experimental.Animations;
+using UnityEngine;
+
+public class IcicleSpawn : MonoBehaviour
+{
+
+	public GameObject icicle;
+
+	public Transform icicleSpawn;
+
+	public float fireRate;
+
+	private float nextFire; 
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(icicle, icicleSpawn.position, icicleSpawn.rotation);
+			Destroy(GameObject.Find("Icicle_Main(Clone)"), 1);
+		}
+	}
+	
+}
