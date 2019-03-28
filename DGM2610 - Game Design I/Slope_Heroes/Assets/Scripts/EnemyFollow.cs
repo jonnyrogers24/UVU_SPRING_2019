2c818b48f09a6b3156135c9293de3eacf709b951
Unknown <jonnyrogersanimation@gmail.com>
2019-03-28 14:41:55 -0600

    Working on Slope Heroes

--- DGM2610 - Game Design I/Slope_Heroes/Assets/Scripts/EnemyFollow.cs
+++ DGM2610 - Game Design I/Slope_Heroes/Assets/Scripts/EnemyFollow.cs
@@ -10,14 +10,9 @@ public class EnemyFollow : MonoBehaviour
 	
 	public float enemySpeed;
 
-	public float maxSpeed = 15;
-
 	void Update ()
 	{
-		if (enemySpeed < maxSpeed)
-		{	
 		transform.position = Vector3.MoveTowards(transform.position, target.transform.position, enemySpeed * Time.deltaTime);
-		}
 	}
 
 }
