80b22f6da4353c8c516959269a032e924d123eab
Unknown <jonnyrogersanimation@gmail.com>
2019-04-01 17:14:36 -0600

    added shield Script

---
+++ DGM2610 - Game Design I/Slope_Heroes/Assets/PickUpScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour
+{
+
+    public GameObject shieldUI;
+    public GameObject shield;
+    public GameObject shieldPickUp;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            shieldPickUp.SetActive(false);
+            shieldUI.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        if (shieldUI.activeSelf == true)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                shield.SetActive(true);
+                
+                StartCoroutine(ShieldOff());
+            }
+            
+            else
+            {
+                print("No Shield");
+            }
+        }
+    }
+
+    IEnumerator ShieldOff()
+    {
+        {
+            yield return new WaitForSeconds(5);
+
+            shield.SetActive(false);
+        }
+    }
+}
