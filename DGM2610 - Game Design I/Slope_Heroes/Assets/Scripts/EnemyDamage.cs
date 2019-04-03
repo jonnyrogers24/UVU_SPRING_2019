660672ae81493240a4dbbf626ee23c283d6f99f0
Unknown <jonnyrogersanimation@gmail.com>
2019-04-03 16:38:18 -0600

    added health to player, added health bar to UI, enemy deals damage

---
+++ DGM2610 - Game Design I/Slope_Heroes/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+	public GameObject Player; 
+	private PlayerHealth playerHealthScript; 
+	public float damage = 10;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			PlayerHealth p = Player.GetComponent<PlayerHealth>();
+			p.TakeDamage(damage);
+		}
+	}
+	
+}
---
+++ DGM2610 - Game Design I/Slope_Heroes/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private float health;
+    public float startHealth = 100;  
+    public Image healthBar;
+
+    private void Start()
+    {
+        health = startHealth; 
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        healthBar.fillAmount = health / startHealth;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+
+    }
+
+    void Die()
+    {
+            Destroy(gameObject);
+    }
+    
+}
+    
+
