8c701cdab5b87ef503a7645696b7a54281c0f9bf
Unknown <jonnyrogersanimation@gmail.com>
2019-04-09 11:04:17 -0600

    Added spike trigger and scripted to restart game when entering.

---
+++ DGM2610 - Game Design I/Slope_Heroes/Assets/Scripts/SpikeTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikeTrigger : MonoBehaviour
+{
+	//public Transform Player;
+	//public Transform RespawnPoint; 
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		
+		if (other.gameObject.CompareTag("Player"))
+		
+			{
+				GetComponent<Animation>().Play();
+
+				StartCoroutine(Respawn());
+			}
+			
+	}
+
+	IEnumerator Respawn()
+	{
+		{
+			
+			yield return new WaitForSeconds(.1f);
+			//Player.transform.position = RespawnPoint.transform.position; 
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+		}
+	}
+	
+}
