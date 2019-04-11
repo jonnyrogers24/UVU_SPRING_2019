df8053eb6e67ea7b88fbb021b4cd0af8a6ddf44b
Unknown <jonnyrogersanimation@gmail.com>
2019-04-11 00:38:34 -0600

    save

---
+++ DGM2610 - Game Design I/Worm_Game/Assets/BoundrySetActive.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundrySetActive : MonoBehaviour
+{
+    public GameObject Boundry;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       Boundry.SetActive(true);
+    }
+
+}
