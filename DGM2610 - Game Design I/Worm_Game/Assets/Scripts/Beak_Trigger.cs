583fda5044caa8211deef443bb5fed4a6a0e3ec9
Unknown <jonnyrogersanimation@gmail.com>
2019-04-22 23:53:37 -0600

    added particle system trigger

--- DGM2610 - Game Design I/Worm_Game/Assets/Scripts/Beak_Trigger.cs
+++ DGM2610 - Game Design I/Worm_Game/Assets/Scripts/Beak_Trigger.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class Beak_Trigger : MonoBehaviour
 {
-	public BeakAnimation anim; 
-    private void OnTriggerEnter(Collider other)
-    {
-		
-        if (other.gameObject.CompareTag("Player"))
+	public BeakAnimation anim;
+	public GameObject beak;
 
-        {
+	private void OnTriggerEnter(Collider other)
+	{
 
-	        anim.BeakPlay(); 
-        }
-			
-    }
+		if (other.gameObject.CompareTag("Player"))
+
+		{
+
+			StartCoroutine(waitBeak());
+		}
+
+	}
+
+	IEnumerator waitBeak()
+	{
+		yield return new WaitForSeconds(1);
+		anim.BeakPlay();
+
+		StartCoroutine(setInactive());
+	}
+
+	IEnumerator setInactive()
+	{
+		yield return new WaitForSeconds(1);
+		beak.SetActive(false);
+	}
 }
---
+++ DGM2610 - Game Design I/Worm_Game/Assets/dirtEmission.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dirtEmission : MonoBehaviour
+{
+
+    public Transform dirt; 
+   
+    void Start()
+    {
+        dirt.GetComponent<ParticleSystem>().Stop();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        dirt.GetComponent<ParticleSystem>().Play();
+        StartCoroutine(stopDirt());
+    }
+
+    IEnumerator stopDirt()
+    {
+        yield return new WaitForSeconds(1f);
+        dirt.GetComponent<ParticleSystem>().Stop();
+    }
+}
