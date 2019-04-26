7c30178fbdfdff1724fea394934298ba2c483b52
Unknown <jonnyrogersanimation@gmail.com>
2019-04-25 21:58:16 -0600

    Animations functional and Health system working

---
+++ DGM2610 - Game Design I/Worm_Game/Assets/Scripts/DamageSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEngine;
+
+public class DamageSystem : MonoBehaviour
+{
+    [SerializeField] private float beakDamage;
+    [SerializeField] private HealthSystem healthController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Damage();
+        }
+    }
+
+    void Damage()
+    {
+        healthController.playerHealth = healthController.playerHealth - beakDamage; 
+        healthController.UpdateHealth();
+    }
+}
---
+++ DGM2610 - Game Design I/Worm_Game/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HealthSystem : MonoBehaviour
+{
+    public float playerHealth;
+    [SerializeField] private Text healthText;
+
+    public void UpdateHealth()
+    {
+        healthText.text = playerHealth.ToString("0");
+        
+        if (playerHealth <= 0)
+        {
+            SceneManager.LoadScene("Worm_Game");
+        }
+    }
+
+}
