873ba961074fc95475a51bba0212358e13dad17e
Unknown <jonnyrogersanimation@gmail.com>
2019-04-29 10:32:26 -0600

    Worm_Game UI and Menus

--- DGM2610 - Game Design I/Worm_Game/Assets/Scripts/HealthSystem.cs
+++ DGM2610 - Game Design I/Worm_Game/Assets/Scripts/HealthSystem.cs
@@ -9,14 +9,30 @@ public class HealthSystem : MonoBehaviour
     public float playerHealth;
     [SerializeField] private Text healthText;
 
+    public GameObject restartButton;
+    public GameObject mainMenuButton; 
+
     public void UpdateHealth()
     {
         healthText.text = playerHealth.ToString("0");
         
         if (playerHealth <= 0)
         {
-            SceneManager.LoadScene("Worm_Game");
+            
+            restartButton.SetActive(true);
+            mainMenuButton.SetActive(true);
+            
         }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 
 }
