52074ebbdf334fb7b312927cba0129a2866a20e8
Unknown <jonnyrogersanimation@gmail.com>
2019-05-02 09:40:09 -0600

    Finished worm game

---
+++ DGM2610 - Game Design I/Worm_Game/Assets/WinScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScript : MonoBehaviour
+{
+    public GameObject restartButton;
+    public GameObject mainMenuButton; 
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            restartButton.SetActive(true);
+            mainMenuButton.SetActive(true);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
+}
